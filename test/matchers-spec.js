ab1027935c201c1d2a55bb2e8bb6c69c8688c159
Wolfram Kriesing <wk@uxebu.com>
2012-04-26 13:53:42 +0200

    Added a bunch of simple test cases.

--- test/matchers-spec.js
+++ test/matchers-spec.js
@@ -49,4 +49,64 @@ require([
 
   });
 
+  describe('toBeOfType', function() {
+
+    it('should work for `number`', function() {
+      expect(1).toBeOfType('number');
+    });
+
+  });
+
+  describe('toHaveLength', function() {
+
+    it('should work for `{length:null}`', function() {
+      expect({length:null}).toHaveLength(null);
+    });
+
+  });
+
+  describe('toHaveProperties', function() {
+
+    it('should work for `{x:0, y:undefined}`', function() {
+      var obj = {x:0, y:undefined};
+      expect(obj).toHaveProperties('x', 'y');
+    });
+
+  });
+
+  describe('toHaveOwnProperties', function() {
+
+    it('should work for `{x:0, y:undefined}`', function() {
+      var obj = {x:0, y:undefined};
+      expect(obj).toHaveOwnProperties('x', 'y');
+    });
+
+  });
+
+  describe('toThrowInstanceOf', function() {
+
+    it('should work for `Error`', function() {
+      expect(function(){ throw new Error; })
+        .toThrowInstanceOf(Error);
+    });
+
+  });
+
+  describe('toHaveBeenCalledXTimes', function() {
+
+    it('should work for 1', function() {
+      var func = jasmine.createSpy('func');
+      func();
+      expect(func).toHaveBeenCalledXTimes(1);
+    });
+
+    it('should work for 2', function() {
+      var func = jasmine.createSpy('func');
+      func();
+      func();
+      expect(func).not.toHaveBeenCalledXTimes(1);
+    });
+
+  });
+
 });
