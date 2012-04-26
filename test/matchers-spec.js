97b12efdb335c8a5950e7522b7be1e8939d8dfad
Wolfram Kriesing <wk@uxebu.com>
2012-04-26 12:46:37 +0200

    Added first tests.

--- test/matchers-spec.js
+++ test/matchers-spec.js
@@ -2,18 +2,51 @@ require([
   './runner.js'
 ], function() {
 
-  describe('matchers', function() {
-    var matchers;
+  describe('toBeArray', function() {
 
-    beforeEach(function() {
-      matchers = this.matchersClass.prototype;
+    describe('matches', function() {
+      it('should pass for []', function() {
+        expect([]).toBeArray();
+      });
+      it('should pass for `new Array`', function() {
+        expect(new Array).toBeArray();
+      });
+      it('should pass for [1,"",{}]', function() {
+        expect([1,"",{}]).toBeArray();
+      });
     });
 
-    it('should be   ', function() {
-      matchers.actual = null;
-      var actual = matchers.toBeArray.apply(null, []);
-      expect(actual).toBe(true);
+    describe('.not matches', function() {
+
+      it('should pass for {}', function() {
+        expect({}).not.toBeArray();
+      });
+      it('should pass for `arguments`', function() {
+        expect(arguments).not.toBeArray();
+      });
+
     });
 
   });
+
+  describe('toBeInstanceOf', function() {
+
+    describe('matches', function() {
+
+      it('should work for `function(){}` to be instance of `Function`', function() {
+        expect(function(){}).toBeInstanceOf(Function);
+      });
+
+    });
+
+  });
+
+  describe('toBeNan', function() {
+
+    it('should work for NaN', function() {
+      expect(NaN).toBeNan();
+    });
+
+  });
+
 });
