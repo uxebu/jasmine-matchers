23e3f6f59966e32232ae7eecb74d7fb8a81c236b
Tobias von Klipstein <klipstein@uxebu.com>
2013-02-02 23:40:32 +0100

    Use Grunt tasks for testing / building / ...

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        eqnull: true,
+        browser: true,
+        newcap: false,
+        predef: [
+          'afterEach',
+          'beforeEach',
+          'describe',
+          'expect',
+          'it',
+          'jasmine',
+          'require',
+          'spyOn',
+          'console',
+          'runs',
+          'xdescribe',
+          'xit',
+          'waitsFor'
+        ]
+      },
+      all: ['Grunfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    jasmine: {
+      test: {
+        src: ['src/**/*.js', '!src/matchers.js'],
+        options: {
+          host: 'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/',
+          outfile: '_spec_runner.html',
+          specs: ['test/**/*-spec.js'],
+          template: require('grunt-template-jasmine-requirejs')
+        }
+      }
+    },
+    jasmine_node: {
+      projectRoot: 'test',
+      requirejs: 'test/require-config.js',
+      forceExit: true
+    },
+    connect: {
+      'test': {
+        options: {
+          hostname: 'localhost',
+          port: 8231
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: ['src/**/*.js', '!src/matchers.js'],
+        dest: 'dist/jasmine-matchers.js'
+      }
+    },
+    watch: {
+      files: '<%= jshint.all %>',
+      tasks: ['default']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jasmine-node');
+
+  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('release', ['concat']);
+  grunt.registerTask('server', ['connect', 'jasmine:test:build', 'watch']);
+  grunt.registerTask('test', ['jasmine_node']);
+  grunt.registerTask('test-phantom', ['connect:test', 'jasmine:test']);
+};
--- Makefile
+++
@@ -1,6 +0,0 @@
-WORKSPACE=${CURDIR}
-DIST_DIR=${WORKSPACE}/dist
-
-build:
-	mkdir -p ${DIST_DIR}
-	find ${WORKSPACE}/src -name *.js | xargs cat > ${DIST_DIR}/matchers.js
